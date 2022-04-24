2e8b3ff2a1e6c15dcf33915757faa71d18f2d814
BYassine2000 <enissa999@gmail.com>
2022-04-24 17:11:01 +0200

    ajout

---
+++ src/DataFixtures/BiensFixtures.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Biens;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class BiensFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        //
+    }
+}
